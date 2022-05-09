2ef44761a8a3f9e47907eeb67e1c114d388dcf08
Arash Sa <101516570+Arashsa31@users.noreply.github.com>
2022-05-09 15:42:52 -0400

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+/**
+ * @author Arash
+ *
+ */
+public class Number 
+{
+	public static void main(String[] args) 
+	{
+		//take user input
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Please enter hours worked:");
+		double hours = keyboard.nextDouble();
+		System.out.print("Please enter wage per hour:");
+		double wage = keyboard.nextDouble();
+		
+		//prints total wage
+		System.out.printf("Your total wage is %.2f.", paycheck(hours, wage));
+		
+		//close scanner object
+		keyboard.close();		
+	}
+	
+    static double paycheck(double hours, double wage) 
+    {
+    	double paycheck, overtimeRate, overtimeHours;
+    	overtimeRate = 1.5;
+    	overtimeHours = 40;
+    	
+    	//check if worked less or equal to 40 hours
+    	if (hours <= overtimeHours) 
+    		{
+	    		//calculate base pay
+	    		paycheck = hours * wage;
+    		}
+        else
+        	{
+	        	//calculate overtime pay
+	        	paycheck = (hours * wage) + (wage * overtimeRate) * (hours - overtimeHours);
+        	}
+        return paycheck;
+    }
+	
+}
